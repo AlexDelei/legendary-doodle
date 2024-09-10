d4be1a9c5cc4f38eb0abb6aeaa86e8a75b9977e8
AlexDelei <alexdelei125@gmail.com>
2024-09-10 15:16:29 +0300

    day 2 of arrays algorithms

--- 0x04-Arrays_dsa/arrays_py.py
+++ 0x04-Arrays_dsa/arrays_py.py
@@ -4,6 +4,11 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+        '''
         for i in range(0, len(nums)):
             first = nums[i]
             slicedArray = nums[i + 1:]
@@ -14,12 +19,30 @@ class Solution:
 
 
 inst = Solution()
-print(inst.twoSum([2, 7, 2, 11], 4))
+# print(inst.twoSum([2, 7, 2, 11], 4))
 
-'''
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+import math
 
-You can return the answer in any order.
-'''
+class Solution2:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        '''
+        Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+        The overall run time complexity should be O(log (m+n)).
+        '''
+        merged = nums1 + nums2
+        merged.sort()
+
+        len_ = len(merged)
+
+        for i in range(0, len_):
+            if len_ % 2 == 0:
+                halfArrayIdx = int(len_ / 2)
+                median = (merged[halfArrayIdx] + merged[halfArrayIdx - 1]) / 2
+                return median
+            else:
+                medianIdx = math.floor(len_ / 2)
+                return merged[medianIdx]
+
+median = Solution2()
+print(median.findMedianSortedArrays([-1, -2], [3]))
